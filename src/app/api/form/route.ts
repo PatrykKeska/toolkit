674d7b74e851ae0813f22e98457d0619ebb65d0b
Patryk Keska <PatrykKeska4@gmail.com>
2023-08-17 20:18:16 +0200

    cors

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -7,10 +7,18 @@ export async function POST(req: NextRequest, res: NextResponse) {
         message: "Wiadomość zostła wysłana. Dziękujemy!",
         success: true,
       }),
+      {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers":
+            "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+      },
     );
   }
 
-  const origin = req.headers.get("origin");
   const body = await req.json();
   console.log(body);
 
@@ -22,7 +30,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": origin || "*" || "http://127.0.0.1:5500",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers":
           "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
