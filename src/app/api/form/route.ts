41e36dfae8b0e7aa84c48278b55b7b1e50470ad2
Patryk Keska <PatrykKeska4@gmail.com>
2023-08-17 20:41:57 +0200

    cors

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -2,21 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   if (req.method === "OPTIONS") {
-    return new Response(
-      JSON.stringify({
-        message: "Wiadomość zostła wysłana. Dziękujemy!",
-        success: true,
-      }),
-      {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers":
-            "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      },
-    );
+    res.headers.set("Access-Control-Allow-Origin", "*");
   }
 
   const body = await req.json();
